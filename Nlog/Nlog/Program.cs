9dbade5bf2d7d47a763f819c7cc4d13c3b4c3c97
Suraj Dhabale <surajdhabale60@gmail.com>
2022-11-27 12:48:47 +0530

    [Suraj]Add:Nlog

---
+++ Nlog/Nlog/Program.cs
@@ -0,0 +1,14 @@
+﻿using NlogProject;
+
+namespace NLogProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0, b = 1;
+            AddNumber add = new AddNumber();
+            Console.WriteLine("Result is : " + add.Sum(a,b));
+        }
+    }
+}
